f67f714fd32b37c7cd45f47304312da3ede1d647
Kaosu-Niku <s9613010@gmail.com>
2022-09-05 04:10:13 +0800

    掉落物

--- Assets/Script/DropAward/DropAward.cs
+++ Assets/Script/DropAward/DropAward.cs
@@ -9,10 +9,43 @@ public abstract class DropAward : PoolObject
     protected override IEnumerator Doing()
     {
         yield return 0;
-        transform.rotation = Quaternion.Euler(0, 0, Random.Range(6, 31) * 5);
-        transform.Translate(1.5f, 0, 0);
-        GetComponent<Rigidbody2D>().AddForce(transform.right * Random.Range(30, 50));
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(4, 33) * 5);
+        float a = Random.Range(3, 8);
+        transform.Translate(a, 0, 0);
+        MovePos = transform.position;
+        transform.Translate(-a, 0, 0);
+        while (Vector3.Distance(transform.position, MovePos) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, MovePos, Time.deltaTime);
+            yield return 0;
+        }
+        StartCoroutine(MoveIEnum());
         yield return new WaitForSeconds(CloseTime);
+    }
+    IEnumerator MoveIEnum()
+    {
+        bool dir = true;
+        float y = Random.Range(0.2f, 0.5f);
+        Vector3 high = transform.position + Vector3.up * y;
+        Vector3 low = transform.position + Vector3.up * -y;
+        while (true)
+        {
+            transform.Rotate(0, 0, -20 * Time.deltaTime);
+            if (dir == true)
+            {
+                transform.position = Vector3.Lerp(transform.position, high, 2 * Time.deltaTime);
+                if (Vector3.Distance(transform.position, high) < 0.1f)
+                    dir = false;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, low, 2 * Time.deltaTime);
+                if (Vector3.Distance(transform.position, low) < 0.1f)
+                    dir = true;
+            }
+            yield return 0;
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
